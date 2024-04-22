eccaefa4cacb0c0075d73c318d04bb632d275d16
EnjiG <257056@glenbrook225.org>
2024-04-22 11:11:35 -0500

    apr22

--- myScripts.js
+++ myScripts.js
@@ -1,5 +1,32 @@
 
-//CLOCK SCRIPT
+  
+ //POST-IT NOTE SAVE (JAVASCRIPT)
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+ }
+  
+ function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+ }
+ 
+ //POST-IT NOTE SHOW AND HIDE
+ var show = false;
+ function showNote(){
+    if (show == false){
+        document.getElementById("note").style.opacity = 1;
+        show = true;
+    }
+    else if (show == true){
+        document.getElementById("note").style.opacity = 0;
+        show = false;
+    }
+ }
+ 
+ //MUST set “opacity:0” in CSS on #note
+ 
+ //CLOCK SCRIPT
 function startTime() {
     const today = new Date();
     let h = today.getHours();
@@ -15,15 +42,5 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
    }
-  
-   //POST-IT NOTE (JAVASCRIPT)
-if (localStorage["note"]) {
-    var note = localStorage["note"];
-    document.getElementById("note").value = note;
-   }
    
-   function autoSave() {
-    var note = document.getElementById("note").value;
-    localStorage.setItem("note", note);
-   }
    
